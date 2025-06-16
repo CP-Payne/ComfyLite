d3c4eb5060bce58075eecebc24ad30360072cfcc
Charles <charlethical1@gmail.com>
2025-06-16 15:38:55 +0200

    feat: implement environment variables

--- cmd/comfylite/main.go
+++ cmd/comfylite/main.go
@@ -14,18 +14,26 @@ import (
 	"github.com/CP-Payne/comfylite/internal/workflow"
 	"github.com/go-chi/chi/v5"
 	"github.com/google/uuid"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Warning: .env file not found, using default values or environment variables.")
+	}
+
+	comfyLiteAddr := GetEnvOrDefault("COMFYLITE_ADDRESS", ":8083")
+	comfyUIAddr := GetEnvOrDefault("COMFYUI_ADDRESS", "http://127.0.0.1:8000")
 
 	ctx := context.Background()
 	clientID := uuid.New()
 
 	manager := workflow.NewManager("templates", "configs")
-
 	webhookNotifier := notifier.NewHTTPNotifier()
 
-	comfyClient := comfy.NewClient("http://127.0.0.1:8000", clientID.String())
+	comfyClient := comfy.NewClient(comfyUIAddr, clientID.String())
 	eventChan := make(chan tracker.Event, 100)
 
 	tracker := tracker.New(webhookNotifier)
@@ -37,18 +45,15 @@ func main() {
 	}
 
 	service := service.NewService(manager, comfyClient, tracker)
-
 	handler := api.NewHandler(service)
 
 	r := chi.NewRouter()
 	r.Post("/generate", handler.HandleGenerateImage)
 
-	log.Println("Starting ComfyLite server on :8083")
-	if err := http.ListenAndServe(":8083", r); err != nil {
+	log.Printf("Starting ComfyLite server on %s\n", comfyLiteAddr)
+	if err := http.ListenAndServe(comfyLiteAddr, r); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
-
-	// TODO: Add .env
 
 }
 
